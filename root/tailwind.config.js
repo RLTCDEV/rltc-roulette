41c095fd66c0fc946556bba62d26bf20d6a650d2
RLTCDEV <RLTCDEV@proton.me>
2025-05-15 00:28:42 -0700

    Update tailwind.config.js

--- root/tailwind.config.js
+++ root/tailwind.config.js
@@ -1,1 +1,11 @@
-
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 6s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
